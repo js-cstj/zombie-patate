6888141d1f5c94980fd43663621391cb32a885ed
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-09-19 18:42:48 -0400

    Départ

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App {
 	 */
 	static main() {
 		var app = document.getElementById("app");
-		
+
 		document.getElementById("btnMettreAJour").addEventListener("click", e => {
 			this.mettreAJour();
 		});
@@ -23,50 +23,22 @@ export default class App {
 		//===========================================================
 		// METTRE À JOUR LES YEUX
 		//===========================================================
-		var inYeux = form.inYeux;
-		var no = inYeux.value;
-		var yeux = document.getElementById("yeux");
-		yeux.src = "images/yeux"+no+".png";
 		
 		//===========================================================
 		// METTRE À JOUR LE NEZ
 		//===========================================================
-		var inNez = form.inNez;
-		var taille = inNez.value;
-		var nez = document.getElementById("nez");
-		nez.src = "images/nez_"+taille+".png";
 
 		//===========================================================
 		// METTRE À JOUR LE CHAPEAU
 		//===========================================================
-		var inChapeau = form.inChapeau;
-		var type = inChapeau.value;
-		var chapeau = document.getElementById("chapeau");
-		chapeau.src = "images/chapeau_"+type+".png";
 
 		//===========================================================
 		// METTRE À JOUR LA BOUCHE
 		//===========================================================
-		var inTirer = form.inTirer;
-		var tirer = inTirer.checked;
-		var bouche = document.getElementById("bouche");
-		if (tirer === true) {
-			bouche.src = "images/bouche_langue.png";
-		} else {
-			bouche.src = "images/bouche.png";
-		}
 
 		//===========================================================
 		// METTRE À JOUR LA BARBE
 		//===========================================================
-		var inBarbe = form.inBarbe;
-		var couleur = inBarbe.value;
-		var barbe = document.getElementById("barbe");
-		if (couleur !== "") {
-			barbe.src = "images/barbe_"+couleur+".png";
-		} else {
-			barbe.src = "images/vide.png";
-		}
 		 
 	}
 	/**
